b0e7ec7c0f17af4dfb25961cc082ab436be22052
Krishna Rajendran <krishna@emptybox.org>
2021-03-11 08:42:46 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Small example to demonstrate painfully slow compile times with rust async.
+
+See src/main.rs
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::future::Future;
+
+macro_rules! compose_middleware_inner {
+  ( $route:ident, $first:ident, $second:ident, $($tail:ident), +) => {
+    $first(|| async {
+        compose_middleware_inner!($route, $second, $($tail),+)
+    }).await
+  };
+  ( $route: ident, $first:ident, $second:ident ) => {
+    $first(|| async move { $second($route).await }).await
+  };
+}
+
+macro_rules! compose_middleware {
+    ( $name:ident, $($tail:ident), +) => {
+        pub async fn $name<N, Fut>(route: N)
+        where
+            N: FnOnce() -> Fut,
+            Fut: Future<Output = ()>,
+        {
+            compose_middleware_inner!(route, $($tail),+)
+        }
+    }
+}
+
+async fn log<N, Fut>(next: N)
+where
+    N: FnOnce() -> Fut,
+    Fut: Future<Output = ()>,
+{
+    println!("log start");
+    next().await;
+    println!("log end");
+}
+
+async fn route() {}
+
+/// Replacing the macro with its expanded form still results in huge compile times.
+/// The macro will generate a function that looks like this:
+/// 
+/// pub async fn my_middleware<N, Fut>(route: N)
+/// where
+///     N: FnOnce() -> Fut,
+///     Fut: Future<Output = ()>,
+/// {
+///     log(|| async { log(|| async { log(|| async move { log(route).await }).await }).await }).await
+/// }
+compose_middleware!(
+    my_middleware,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log,
+    log
+);
+
+#[tokio::main]
+async fn main() {
+    my_middleware(route).await;
+}
