23b511148130b859a3552cdffe15fbb2c9eee592
Krishna Rajendran <krishna@emptybox.org>
2021-03-11 10:41:14 -0800

    Change project to a lib with no dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,16 @@ macro_rules! compose_middleware_inner {
   };
 }
 
+/// Replacing the macro with its expanded form still results in huge compile times.
+/// The macro will generate a function that looks like this:
+/// 
+/// pub async fn my_middleware<N, Fut>(route: N)
+/// where
+///     N: FnOnce() -> Fut,
+///     Fut: Future<Output = ()>,
+/// {
+///     log(|| async { log(|| async { log(|| async move { log(route).await }).await }).await }).await
+/// }
 macro_rules! compose_middleware {
     ( $name:ident, $($tail:ident), +) => {
         pub async fn $name<N, Fut>(route: N)
@@ -33,18 +43,6 @@ where
     println!("log end");
 }
 
-async fn route() {}
-
-/// Replacing the macro with its expanded form still results in huge compile times.
-/// The macro will generate a function that looks like this:
-/// 
-/// pub async fn my_middleware<N, Fut>(route: N)
-/// where
-///     N: FnOnce() -> Fut,
-///     Fut: Future<Output = ()>,
-/// {
-///     log(|| async { log(|| async { log(|| async move { log(route).await }).await }).await }).await
-/// }
 compose_middleware!(
     my_middleware,
     log,
@@ -63,8 +61,3 @@ compose_middleware!(
     log,
     log
 );
-
-#[tokio::main]
-async fn main() {
-    my_middleware(route).await;
-}
